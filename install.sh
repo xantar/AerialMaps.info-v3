00ff2e4c7ecce9e5d63657b324af5fe90f17cc92
Ubuntu <ubuntu@ip-172-31-4-252.us-west-1.compute.internal>
2015-10-19 18:41:30 +0000

    adjusted install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
-sudo apt-get update
-sudo apt-get upgrade
+echo "-- Installing RVM --"
+
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
+
 source /home/ubuntu/.rvm/scripts/rvm
 
+echo "Installing Rails"
 gem install rails
 
 sudo apt-get install imagemagick hugin vim zip nodejs libpq-dev rawtherapee sqlitebrowser
+
+echo "configuring Rails App"
 
 bundle install
 
