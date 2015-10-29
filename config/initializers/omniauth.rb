69adb2aa1ba860b0433fcc778e177f07704be60d
Ubuntu <ubuntu@ip-172-31-11-130.us-west-1.compute.internal>
2015-10-29 22:28:20 +0000

    Fixed Google+ auth & added logo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, "1485599321766492", "b5d34efbf004792219a61b1404a8d0de",
            scope: 'public_profile', info_fields: 'id,name,link'
 
-  provider :google_oauth2, "501070631545-92oq7svi02fd4b7vi0fkv1mpgr5fovkh.apps.googleusercontent.com", "wEkDxw8SoDAPEwIdvs7Np_6F",
+  provider :google_oauth2, "501070631545-jqfmen2ll2p2i9p4hgd4nmpaa46n9dms.apps.googleusercontent.com", "2sEFfy6hkH5z89_jb_S7b51H",
            scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
 
   provider :github, "1f4083736c6a53b94bf8", "86c47339a073018f04feb18004eedd045f90f082"
